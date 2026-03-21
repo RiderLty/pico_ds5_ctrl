151c35c10939205166162c4c82ad81829bd89554
RiderLty <lty6531600@gmail.com>
2026-03-21 19:39:51 +0800

    feat: 添加键鼠映射速度配置持久化功能
    添加pico-kvstore依赖用于存储键鼠映射速度配置
    实现速度配置的读取和保存功能
    优化鼠标移动处理逻辑，支持更平滑的移动
    增加触摸板速度调节功能

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 将DS5手柄通过树莓派pico2w转换为键鼠输出
 
 已知问题：会偶发性随机卡死，不确定原因
+
+mkdir lib && cd lib && git clone --depth 1  --recursive https://github.com/oyama/pico-kvstore
--- main.cpp
+++ main.cpp
@@ -32,6 +32,11 @@
 #include "pico/stdlib.h"
 #include "pico/multicore.h"
 #include "pico/mutex.h"
+extern "C" {
+    #define delete delete_func 
+    #include "kvstore.h"
+    #undef delete 
+}
 
 #include "utils.h"
 #include "bt.h"
@@ -47,20 +52,13 @@
 
 static uint8_t ready = 0;
 
-enum  {
-  BLINK_NOT_MOUNTED = 250,
-  BLINK_MOUNTED = 1000,
-  BLINK_SUSPENDED = 2500,
-};
 
-static uint32_t blink_interval_ms = BLINK_NOT_MOUNTED;
-
-char display_buffer[500];
 static struct dualsense_input_report ds ,ds_now, ds_last;
-void display_dsinfo(){
-  sprintf(display_buffer, "x:%03d y:%03d rx:%03d ry:%03d lt:%03d rt:%03d a:%d b:%d y:%d x:%d lb:%d rb:%d lt:%d rt:%d back:%d start:%d ls:%d rs:%d ps:%d touchpad:%d mute:%d dpad:%d gyro_x:%08d gyro_y:%08d gyro_z:%08d accel_x:%08d accel_y:%08d accel_z:%08d sensor_timestamp:%08d touchpad: points_1[%08b]:%06d,%06d points_2[%08b]:%06d,%06d\r\n\0", ds.ls_x, ds.ls_y, ds.rs_x, ds.rs_y, ds.lt, ds.rt, ds.buttons.a, ds.buttons.b, ds.buttons.y, ds.buttons.x, ds.buttons.lb, ds.buttons.rb, ds.buttons.lt, ds.buttons.rt, ds.buttons.back, ds.buttons.start, ds.buttons.ls, ds.buttons.rs, ds.buttons.ps, ds.buttons.touchpad, ds.buttons.mute, ds.buttons.dpad, ds.gyro_x, ds.gyro_y, ds.gyro_z, ds.accel_x, ds.accel_y, ds.accel_z, ds.sensor_timestamp, ds.points_1.contact, ds.points_1.x, ds.points_1.y, ds.points_2.contact, ds.points_2.x, ds.points_2.y);
-  uart_puts(UART_ID, display_buffer);
-}
+// char display_buffer[500];
+// void display_dsinfo(){
+//   sprintf(display_buffer, "x:%03d y:%03d rx:%03d ry:%03d lt:%03d rt:%03d a:%d b:%d y:%d x:%d lb:%d rb:%d lt:%d rt:%d back:%d start:%d ls:%d rs:%d ps:%d touchpad:%d mute:%d dpad:%d gyro_x:%08d gyro_y:%08d gyro_z:%08d accel_x:%08d accel_y:%08d accel_z:%08d sensor_timestamp:%08d touchpad: points_1[%08b]:%06d,%06d points_2[%08b]:%06d,%06d\r\n\0", ds.ls_x, ds.ls_y, ds.rs_x, ds.rs_y, ds.lt, ds.rt, ds.buttons.a, ds.buttons.b, ds.buttons.y, ds.buttons.x, ds.buttons.lb, ds.buttons.rb, ds.buttons.lt, ds.buttons.rt, ds.buttons.back, ds.buttons.start, ds.buttons.ls, ds.buttons.rs, ds.buttons.ps, ds.buttons.touchpad, ds.buttons.mute, ds.buttons.dpad, ds.gyro_x, ds.gyro_y, ds.gyro_z, ds.accel_x, ds.accel_y, ds.accel_z, ds.sensor_timestamp, ds.points_1.contact, ds.points_1.x, ds.points_1.y, ds.points_2.contact, ds.points_2.x, ds.points_2.y);
+//   uart_puts(UART_ID, display_buffer);
+// }
 
 void debug(char *fmt, ...){
   char buffer[500];
@@ -75,7 +73,6 @@ auto_init_mutex(my_mutex);
 
 void on_bt_data(CHANNEL_TYPE channel, uint8_t *data, uint16_t len) {
     if (channel == INTERRUPT && data[1] == 0x31) {
-
       mutex_enter_blocking(&my_mutex);
       memcpy(&ds, data + 3, sizeof(ds));
       mutex_exit(&my_mutex);
@@ -100,17 +97,64 @@ void on_bt_event(BT_EVENT event) {
 static uint8_t last_id1 = 0xFF;
 static uint8_t last_id2 = 0xFF;
 
+void set_speed(float s1, float s2, float s3, float s4){
+    float check_sum = s1 + s2 + s3 + s4;
+    char buffer[sizeof(float)*5];
+    memcpy(buffer, &s1, sizeof(float));
+    memcpy(buffer + sizeof(float), &s2, sizeof(float));
+    memcpy(buffer + sizeof(float) * 2, &s3, sizeof(float));
+    memcpy(buffer + sizeof(float) * 3, &s4, sizeof(float));
+    memcpy(buffer + sizeof(float) * 4, &check_sum, sizeof(float));
+    if (kvs_set("ds_speed", buffer, sizeof(buffer)) != KVSTORE_SUCCESS){
+        debug("set speed error\n");
+        return;
+    }
+    debug("set speed success\n");
+    return;
+}
+
+void get_speed(float *s1, float *s2, float *s3, float *s4){
+    char buffer[sizeof(float)*5];
+    size_t value_size = 0;  
+    if (kvs_get("ds_speed", buffer, sizeof(buffer), &value_size) != KVSTORE_SUCCESS){
+        debug("get speed error\n");
+        return;
+    }
+    float ts1 , ts2, ts3, ts4, check_sum;
+    memcpy(&ts1, buffer, sizeof(float));
+    memcpy(&ts2, buffer + sizeof(float), sizeof(float));
+    memcpy(&ts3, buffer + sizeof(float) * 2, sizeof(float));
+    memcpy(&ts4, buffer + sizeof(float) * 3, sizeof(float));
+    memcpy(&check_sum, buffer + sizeof(float) * 4, sizeof(float));
+    if(check_sum != ts1 + ts2 + ts3 + ts4){
+        debug("check_sum error\n");
+        return;
+    }
+    memcpy(s1, &ts1, sizeof(float));
+    memcpy(s2, &ts2, sizeof(float));
+    memcpy(s3, &ts3, sizeof(float));
+    memcpy(s4, &ts4, sizeof(float));
+    debug("get speed success\n");
+}
+
+
+#define DEFAULT_LS_MAP_WHEEL_SPEED 0.064f // 默认滚轮映射速度系数
+#define DEFAULT_RS_MAP_MOUSE_SPEED 8.0f // 默认鼠标映射速度系数
+#define DEFAULT_TOUCHPAD_MAP_WHEEL_SPEED 0.03f // 默认触摸板映射速度系数
+#define DEFAULT_TOUCHPAD_MAP_MOUSE_SPEED 1.0f // 默认触摸板映射速度系数
+
+#define AXIS_CENTER 128 // 中心位置
+#define AXIS_DEAD_ZONE 6 // 死区
+#define REPORT_RATE 100 // 报告率
+
+static float ls_map_wheel_speed; // 滚轮映射速度系数
+static float rs_map_mouse_speed; // 鼠标映射速度系数
+static float touchpad_map_wheel_speed; // 触摸板映射速度系数
+static float touchpad_map_mouse_speed; // 触摸板映射速度系数
+static absolute_time_t last_write_time; // 上次写入时间
+static bool need_set_speed = false; // 是否需要写入映射速度系数
 void mouse_keyboard_ctr_taskl() { // 鼠标键盘控制任务
     uint32_t counter = 0;
-    #define AXIS_CENTER 128 // 中心位置
-    #define AXIS_DEAD_ZONE 6 // 死区
-    #define REPORT_RATE 100 // 报告率
-    
-    float ls_map_wheel_speed = 0.064f; // 滚轮映射速度系数
-    float rs_map_mouse_speed = 8.0f; // 鼠标映射速度系数
-    float touchpad_map_wheel_speed = 0.03f; // 触摸板映射速度系数
-    float touchpad_map_mouse_speed = 1.0f; // 触摸板映射速度系数
-    
     const uint64_t INTERVAL_US = 100000 / REPORT_RATE;
     absolute_time_t next_run_time = get_absolute_time();
     float current_speed_x = 0;
@@ -119,12 +163,10 @@ void mouse_keyboard_ctr_taskl() { // 鼠标键盘控制任务
     float x_move = 0.0f;
     float y_move = 0.0f;
     float wheel_move = 0.0f;
-
     int32_t ls_x = 0;
     int32_t ls_y = 0;
     int32_t rs_x = 0;
     int32_t rs_y = 0;
-
     while (1) {
         if (ready) {
             counter++;
@@ -163,25 +205,76 @@ void mouse_keyboard_ctr_taskl() { // 鼠标键盘控制任务
             uint8_t dpad_released = ~dapd_now & dapd_last;
 
             if(ds_now.buttons.mute ){//静音键按下的状态
+                bool is_map_data_changed = false; // 是否更新了映射速度系数
                 if(ds_now.buttons.x){
                     rs_map_mouse_speed -=0.005f;
                     rs_map_mouse_speed = rs_map_mouse_speed < 0.005f ? 0.005f : rs_map_mouse_speed;
                     debug("mouse:%.5f\n", rs_map_mouse_speed);
+                    is_map_data_changed = true;
                 }
                 if(ds_now.buttons.b){
                     rs_map_mouse_speed +=0.005f;
-                    rs_map_mouse_speed = rs_map_mouse_speed > 127.0f ? 127.0f : rs_map_mouse_speed;
+                    // rs_map_mouse_speed = rs_map_mouse_speed > 127.0f ? 127.0f : rs_map_mouse_speed;
                     debug("mouse:%.5f\n", rs_map_mouse_speed);
+                    is_map_data_changed = true;
                 }
                 if(ds_now.buttons.a){
                     ls_map_wheel_speed -=0.00005f;
                     ls_map_wheel_speed = ls_map_wheel_speed < 0.0001f ? 0.0001f : ls_map_wheel_speed;
                     debug("wheel:%.5f\n", ls_map_wheel_speed);
+                    is_map_data_changed = true;
                 }
                 if(ds_now.buttons.y){
                     ls_map_wheel_speed +=0.00005f;
-                    ls_map_wheel_speed = ls_map_wheel_speed > 0.5f ? 0.5f : ls_map_wheel_speed;
+                    // ls_map_wheel_speed = ls_map_wheel_speed > 0.5f ? 0.5f : ls_map_wheel_speed;
                     debug("wheel:%.5f\n", ls_map_wheel_speed);
+                    is_map_data_changed = true;
+                }
+                if(dapd_now & DPAD_UP){
+                    touchpad_map_wheel_speed +=0.00005f;
+                    // touchpad_map_wheel_speed = touchpad_map_wheel_speed > 0.5f ? 0.5f : touchpad_map_wheel_speed;
+                    debug("touchpad_wheel:%.5f\n", touchpad_map_wheel_speed);
+                    is_map_data_changed = true;
+                }
+                if(dapd_now & DPAD_DOWN){
+                    touchpad_map_wheel_speed -=0.00005f;
+                    touchpad_map_wheel_speed = touchpad_map_wheel_speed < 0.0001f ? 0.0001f : touchpad_map_wheel_speed;
+                    debug("touchpad_wheel:%.5f\n", touchpad_map_wheel_speed);
+                    is_map_data_changed = true;
+                }
+                if(dapd_now & DPAD_RIGHT){
+                   touchpad_map_mouse_speed +=0.002f;
+                //    touchpad_map_mouse_speed = touchpad_map_mouse_speed > 127.0f ? 127.0f : touchpad_map_mouse_speed;
+                   debug("touchpad_mouse:%.5f\n", touchpad_map_mouse_speed);
+                   is_map_data_changed = true;
+                }
+                if(dapd_now & DPAD_LEFT){
+                   touchpad_map_mouse_speed -=0.002f;
+                   touchpad_map_mouse_speed = touchpad_map_mouse_speed < 0.005f ? 0.005f : touchpad_map_mouse_speed;
+                   debug("touchpad_mouse:%.5f\n", touchpad_map_mouse_speed);
+                   is_map_data_changed = true;
+                }
+                if(!ds_now.buttons.rs && ds_last.buttons.rs){//重设摇杆控制速度
+                    rs_map_mouse_speed = DEFAULT_RS_MAP_MOUSE_SPEED;
+                    ls_map_wheel_speed = DEFAULT_LS_MAP_WHEEL_SPEED;
+                    debug("reset mouse:%.5f wheel:%.5f\n", rs_map_mouse_speed, ls_map_wheel_speed);
+                    is_map_data_changed = true;
+                }
+                if(!ds_now.buttons.ls && ds_last.buttons.ls){//重设触摸板控制速度
+                    touchpad_map_wheel_speed = DEFAULT_TOUCHPAD_MAP_WHEEL_SPEED;
+                    touchpad_map_mouse_speed = DEFAULT_TOUCHPAD_MAP_MOUSE_SPEED;
+                    debug("reset touchpad mouse:%.5f wheel:%.5f\n", touchpad_map_mouse_speed, touchpad_map_wheel_speed);
+                    is_map_data_changed = true;
+                }
+                if(!ds_now.buttons.ps && ds_last.buttons.ps ){//重启
+                    debug("restarting device.\n");
+                    sleep_ms(100);
+                    watchdog_reboot(0, 0, 0);
+                }
+
+                if(is_map_data_changed){
+                    last_write_time = get_absolute_time();
+                    need_set_speed = true;
                 }
             }else{
                 if(ds_now.buttons.rt && !ds_last.buttons.rt){
@@ -214,11 +307,7 @@ void mouse_keyboard_ctr_taskl() { // 鼠标键盘控制任务
                 if(!ds_now.buttons.touchpad && ds_last.buttons.touchpad){
                     hid_mouse_button_up(MouseBtnLeft);
                 }
-                if(ds_now.buttons.ps && !ds_now.buttons.mute && ds_last.buttons.mute){//PS按下且静音键松开 重启
-                    debug("restarting device.\n");
-                    sleep_ms(100);
-                    watchdog_reboot(0, 0, 0);
-                }
+      
                 /*----------DPAD部分----------*/
                 if(dpad_pressed & DPAD_UP){
                 hid_key_down(KeyUp);
@@ -245,9 +334,6 @@ void mouse_keyboard_ctr_taskl() { // 鼠标键盘控制任务
                 hid_key_up(KeyLeft);
                 }
             }
-
-
-           
             /*----------触摸板部分----------*/
             bool state1 = (ds_now.points_1.contact & 0x80) == 0;
             uint8_t id1 = ds_now.points_1.contact & 0x7f;
@@ -288,18 +374,21 @@ void mouse_keyboard_ctr_taskl() { // 鼠标键盘控制任务
             else {
             }
             /*----------OVER----------*/
-            int8_t report_x = (int8_t) x_move;
-            int8_t report_y = (int8_t) y_move;
-            int8_t report_wheel = (int8_t) wheel_move;
-            if (report_x != 0 || report_y != 0 || report_wheel != 0) {
-                int32_t count = hid_mouse_move(report_x, report_y, report_wheel);
-                debug("(%03d,%03d)[%d,%d]\n", report_x, report_y, count, absolute_time_diff_us(next_run_time, get_absolute_time()));
-                // debug("counter:%d & waitcount:%d\n", counter, count);
-                // tud_hid_mouse_report(REPORT_ID_MOUSE, 0x00, report_x, report_y, 0, 0);
-                x_move -= (float) report_x;
-                y_move -= (float) report_y;
-                wheel_move -= (float) report_wheel;
+            while (x_move != 0.0f || y_move != 0.0f || wheel_move != 0.0f) {
+                int8_t report_x = (x_move > 127.0f) ? 127 : (x_move < -127.0f) ? -127 : (int8_t)x_move;
+                int8_t report_y = (y_move > 127.0f) ? 127 : (y_move < -127.0f) ? -127 : (int8_t)y_move;
+                int8_t report_wheel = (wheel_move > 127.0f) ? 127 : (wheel_move < -127.0f) ? -127 : (int8_t)wheel_move;
+                if (report_x != 0 || report_y != 0 || report_wheel != 0) {
+                    int32_t count = hid_mouse_move(report_x, report_y, report_wheel);
+                    // debug("(%d,%d)[%d]\n", report_x, report_y, count);
+                    x_move -= (float)report_x;
+                    y_move -= (float)report_y;
+                    wheel_move -= (float)report_wheel;
+                    // sleep_ms(1); 
+            } else {
+                break;
             }
+        }
             // absolute_time_t finish_time = get_absolute_time();
             // debug("mutex_enter_time:%d exec_time:%d\n", absolute_time_diff_us(start_time, mutex_enter_time), absolute_time_diff_us(finish_time, mutex_enter_time));
         }
@@ -319,6 +408,14 @@ int main(void)
   uart_set_format(UART_ID, 8, 1, UART_PARITY_NONE);
   uart_set_hw_flow(UART_ID, false, false);
   uart_puts(UART_ID, "port open\r\n");
+  kvs_init();
+  ls_map_wheel_speed = DEFAULT_LS_MAP_WHEEL_SPEED; 
+  rs_map_mouse_speed = DEFAULT_RS_MAP_MOUSE_SPEED; 
+  touchpad_map_wheel_speed = DEFAULT_TOUCHPAD_MAP_WHEEL_SPEED; 
+  touchpad_map_mouse_speed = DEFAULT_TOUCHPAD_MAP_MOUSE_SPEED;
+  last_write_time = get_absolute_time();
+  get_speed(&ls_map_wheel_speed, &rs_map_mouse_speed, &touchpad_map_wheel_speed, &touchpad_map_mouse_speed);
+  debug("read ls_map_wheel_speed: %f, rs_map_mouse_speed: %f, touchpad_map_wheel_speed: %f, touchpad_map_mouse_speed: %f\n", ls_map_wheel_speed, rs_map_mouse_speed, touchpad_map_wheel_speed, touchpad_map_mouse_speed);
   tusb_ctrl_init();
   bt_init();
   bt_register_data_callback(on_bt_data);
@@ -328,5 +425,14 @@ int main(void)
   {
     cyw43_arch_poll();
     tud_task(); // tinyusb device task
+    
+    if(need_set_speed){
+        if(absolute_time_diff_us(last_write_time, get_absolute_time()) >= 3 * 1000000){//等待3s后设置速度
+            debug("write ls_map_wheel_speed: %f, rs_map_mouse_speed: %f, touchpad_map_wheel_speed: %f, touchpad_map_mouse_speed: %f\n", ls_map_wheel_speed, rs_map_mouse_speed, touchpad_map_wheel_speed, touchpad_map_mouse_speed);
+            set_speed(ls_map_wheel_speed, rs_map_mouse_speed, touchpad_map_wheel_speed, touchpad_map_mouse_speed);
+            need_set_speed = false;
+
+        }
+    }
   }
 }
